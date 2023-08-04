1df5d44cd525dad60ec9b230c5f1284db00564bd
devnote-dev <devnote.dev75@gmail.com>
2023-08-04 01:57:51 +0100

    feat: implement unsafe load & macro

--- src/license.cr
+++ src/license.cr
@@ -1,3 +1,4 @@
+require "ecr/macros"
 require "yaml"
 
 class License
@@ -10,13 +11,29 @@ class License
   getter spdx_id : String
   getter nickname : String?
   getter description : String
+  @[YAML::Field(key: "using")]
   getter used_by : Hash(String, String)
   # TODO: make these enums
   getter permissions : Array(String)
   getter conditions : Array(String)
   getter limitations : Array(String)
+  @[YAML::Field(ignore: true)]
+  property! body : String
 
-  private def initialize(@title, @spdx_id, @nickname, @description, @used_by,
-                         @permissions, @conditions, @limitations)
+  def self.unsafe_load(source : String)
+    _, data, content = source.split "---\n", 3
+
+    license = from_yaml data
+    license.body = content
+
+    license
+  end
+
+  macro get_by_id(name)
+    {% if file_exists?("./src/licenses/#{name.id}.txt") %}
+      License.unsafe_load {{ read_file("./src/licenses/#{name.id}.txt") }}
+    {% else %}
+      {% raise "Unknown license SPDX-ID: #{name.id}" %}
+    {% end %}
   end
 end
